213dcb3eff53de570f881e251043ee920f3e8a48
Seyedpouya <seyedpouya@digi-conception.fr>
2025-02-02 23:24:48 +0100

    Initial Commit

---
+++ BirthdayCountdownApp/BirthdayCountdownAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayCountdownAppApp.swift
+//  BirthdayCountdownApp
+//
+//  Created by Pouya Sadri on 02/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayCountdownAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
